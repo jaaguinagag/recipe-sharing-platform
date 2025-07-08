404c0f7b74a85a6872564ac1e13ec1914952e0e8
Jordi Aguinaga <jaguinaga@gponutec.com>
2025-07-08 11:59:25 -0600

    Fix Router error

--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { useRouter, useParams } from "next/navigation";
+import { useParams } from "next/navigation";
 import { supabase } from "../../../lib/supabase-client";
 import { Skeleton } from "../../../components/skeleton";
 
